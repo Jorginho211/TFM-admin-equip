54544e833364f18a288d0b5ee1c81f6763ed4d8c
Jorge Lopez <jorge.lopez@riseup.net>
2019-08-08 20:07:04 +0200

    Añadido alert de error en caso de usuario o contraseña invalidos

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,37 @@
 <template>
   <v-container fluid fill-height align-center class="bg_container">
-    <v-card width="500" height="300" class="mx-auto pa-5">
+    <v-card width="500" class="mx-auto pa-5">
       <v-layout column>
-        <v-form ref="form">
-        <v-flex>
-          <v-text-field
-            v-model="username"
-            label="Usuario"
-            placeholder="pepe.perez"
-            :rules="[v => !!v || '']"
-            outlined
-          ></v-text-field>
-        </v-flex>
+        <v-alert v-if="loginError" dense outlined type="error">Usuario ou contrasinal incorrectos</v-alert>
+        <v-form ref="form" @keyup.native.enter="login()">
+          <v-flex mb-5>
+            <v-text-field
+              v-model="username"
+              label="Usuario"
+              placeholder="pepe.perez"
+              :rules="[v => !!v || '']"
+              outlined
+              hide-details
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex>
-          <v-text-field
-            v-model="password"
-            label="Contrasinal"
-            placeholder="Contrasinal"
-            :type="showPassword ? 'text' : 'password'"
-            :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-            outlined
-            :rules="[v => !!v || '']"
-            @click:append="showPassword = !showPassword"
-          ></v-text-field>
-        </v-flex>
+          <v-flex mb-5>
+            <v-text-field
+              v-model="password"
+              label="Contrasinal"
+              placeholder="Contrasinal"
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+              outlined
+              :rules="[v => !!v || '']"
+              @click:append="showPassword = !showPassword"
+              hide-details
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex>
-          <v-btn color="primary" @click="login()">Entrar</v-btn>
-        </v-flex>
+          <v-flex>
+            <v-btn color="primary" @click="login()">Entrar</v-btn>
+          </v-flex>
         </v-form>
       </v-layout>
     </v-card>
@@ -43,7 +46,8 @@ export default {
     return {
       username: "",
       password: "",
-      showPassword: false
+      showPassword: false,
+      loginError: false
     };
   },
   created() {
@@ -54,7 +58,7 @@ export default {
   },
   methods: {
     login() {
-      if(!this.$refs.form.validate()){
+      if (!this.$refs.form.validate()) {
         return;
       }
 
@@ -65,14 +69,18 @@ export default {
         }
       })
         .then(response => {
-          if(Unauthorized(this.$router, response.status)) return;
+          if (Unauthorized(this.$router, response.status)) return;
 
           localStorage.setItem("token", response.data.token);
-          HTTP.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem("token");
+          HTTP.defaults.headers["Authorization"] =
+            "Bearer " + localStorage.getItem("token");
           this.$router.replace({ name: "main" });
         })
         .catch(error => {
-          console.log(error);
+          if (Unauthorized(this.$router, error.response.status)){
+            this.loginError = true;
+            return;
+          }
         });
     }
   }
